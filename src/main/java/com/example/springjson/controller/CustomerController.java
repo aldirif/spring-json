3f64e5f2c7fbf417c9ff48b8cc881d6a40beda45
aldirif <97742642+AldiRif@users.noreply.github.com>
2022-12-22 13:36:42 +0700

    Update

--- src/main/java/com/example/springjson/controller/CustomerController.java
+++ src/main/java/com/example/springjson/controller/CustomerController.java
@@ -1,6 +1,5 @@
 package com.example.springjson.controller;
 
-import com.example.springjson.model.CustomerRequest;
 import com.example.springjson.model.ResponseModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
